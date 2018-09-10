fb812820209d8a6ea2ba8ec6ec7bc1dbc1746b46
fA1sEr <945560537@qq.com>
2018-09-10 14:44:45 +0800

    init

---
+++ Main.py
@@ -0,0 +1,216 @@
+import itertools as it
+import os
+from time import time, sleep
+import numpy as np
+import skimage.color
+import skimage.transform
+import tensorflow as tf
+from tqdm import trange
+from vizdoom import *
+from Agent import Agent
+from GameSimulator import GameSimulator
+
+# to choose gpu
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"
+
+FRAME_REPEAT = 4 # How many frames 1 action should be repeated
+UPDATE_FREQUENCY = 4 # How many actions should be taken between each network update
+COPY_FREQUENCY = 1000
+
+RESOLUTION = (80, 45, 4) # Resolution
+BATCH_SIZE = 32 # Batch size for experience replay
+LEARNING_RATE = 0.001 # Learning rate of model
+GAMMA = 0.99 # Discount factor
+
+MEMORY_CAP = 200000 # Amount of samples to store in memory
+
+EPSILON_MAX = 1 # Max exploration rate
+EPSILON_MIN = 0.05 # Min exploration rate
+EPSILON_DECAY_STEPS = 2e5 # How many steps to decay from max exploration to min exploration
+
+RANDOM_WANDER_STEPS = 200000 # How many steps to be sampled randomly before training starts
+
+TRACE_LENGTH = 8 # How many traces are used for network updates
+HIDDEN_SIZE = 768 # Size of the third convolutional layer when flattened
+
+EPOCHS = 20000000 # Epochs for training (1 epoch = 200 training Games and 10 test episodes)
+GAMES_PER_EPOCH = 200 # How actions to be taken per epoch
+EPISODES_TO_TEST = 10 # How many test episodes to be run per epoch for logging performance
+EPISODE_TO_WATCH = 10 # How many episodes to watch after training is complete
+
+TAU = 0.99 # How much the target network should be updated towards the online network at each update
+
+LOAD_MODEL = False # Load a saved model?
+SAVE_MODEL = True # Save a model while training?
+SKIP_LEARNING = False # Skip training completely and just watch?
+
+max_model_savefile = "train_data/max_model/max_model.ckpt"
+model_savefile = "train_data/model.ckpt" # Name and path of the model
+reward_savefile = "train_data/Rewards.txt"
+
+##########################################
+
+def updateTargetGraph(tfVars,tau):
+    total_vars = len(tfVars)
+    op_holder = []
+    for idx,var in enumerate(tfVars[0:total_vars//2]):
+        op_holder.append(tfVars[idx+total_vars//2].assign((var.value()*tau) + ((1-tau)*tfVars[idx+total_vars//2].value())))
+    return op_holder
+
+def updateTarget(op_holder,sess):
+    for op in op_holder:
+        sess.run(op)
+
+def saveScore(score):
+    my_file = open(reward_savefile, 'a')  # Name and path of the reward text file
+    my_file.write("%s\n" % score)
+    my_file.close()
+
+###########################################
+
+game = GameSimulator()
+game.initialize()
+
+ACTION_COUNT = game.get_action_size()
+
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.33)
+
+SESSION = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+if LOAD_MODEL:
+    EPSILON_MAX = 0.25 # restart after 20+ epoch
+
+agent = Agent(memory_cap = MEMORY_CAP, batch_size = BATCH_SIZE, resolution = RESOLUTION, action_count = ACTION_COUNT,
+            session = SESSION, lr = LEARNING_RATE, gamma = GAMMA, epsilon_min = EPSILON_MIN, trace_length=TRACE_LENGTH,
+            epsilon_decay_steps = EPSILON_DECAY_STEPS, epsilon_max=EPSILON_MAX, hidden_size=HIDDEN_SIZE)
+
+saver = tf.train.Saver()
+
+trainables = tf.trainable_variables()
+
+targetOps = updateTargetGraph(trainables, TAU)
+
+if LOAD_MODEL:
+    print("Loading model from: ", model_savefile)
+    saver.restore(SESSION, model_savefile)
+else:
+    init = tf.global_variables_initializer()
+    SESSION.run(init)
+
+##########################################
+
+if not SKIP_LEARNING:
+    time_start = time()
+    print("\nFilling out replay memory")
+    updateTarget(targetOps, SESSION)
+
+    agent.reset_cell_state()
+    state = game.get_state()
+    for _ in range(RANDOM_WANDER_STEPS):
+        if not LOAD_MODEL:
+            action = agent.random_action()
+        else:
+            action = agent.act(game.get_last_action(), state)
+        img_state, reward, done = game.make_action(action)
+        if not done:
+            state_new = img_state
+        else:
+            state_new = None
+
+        agent.add_transition(state, action, reward, state_new, done)
+        state = state_new
+
+        if done:
+            game.reset()
+            agent.reset_cell_state()
+            state = game.get_state()
+
+    max_avgR = -10000.0
+
+    for epoch in range(EPOCHS):
+        print("\n\nEpoch %d\n-------" % (epoch + 1))
+        print("Training...")
+
+        learning_step = 0
+        for games_cnt in range(GAMES_PER_EPOCH):
+            game.reset()
+            agent.reset_cell_state()
+            state = game.get_state()
+            while True:
+                learning_step += 1
+                action = agent.act(state)
+                img_state, reward, done = game.make_action(action)
+                if not done:
+                    state_new = img_state
+                else:
+                    state_new = None
+                agent.add_transition(state, action, reward, state_new, done)
+                state = state_new
+
+                if learning_step % UPDATE_FREQUENCY == 0:
+                    agent.learn_from_memory()
+                if learning_step % COPY_FREQUENCY == 0:
+                    updateTarget(targetOps, SESSION)
+
+                if done:
+                    print("Epoch %d Train Game %d get %.1f" % (epoch, games_cnt, game.get_total_reward()))
+                    break
+            if SAVE_MODEL and games_cnt % 10 == 0:
+                saver.save(SESSION, model_savefile)
+                print("Saving the network weigths to:", model_savefile)
+
+        print("\nTesting...")
+
+        test_scores = []
+        for test_step in range(EPISODES_TO_TEST):
+            game.reset()
+            agent.reset_cell_state()
+            while not game.is_terminared():
+                state = game.get_state()
+                action = agent.act(state, train=False)
+                game.make_action(action)
+            test_scores.append(game.get_total_reward())
+
+        test_scores = np.array(test_scores)
+        print("Results: mean: %.1f±%.1f," % (test_scores.mean(), test_scores.std()),
+              "min: %.1f" % test_scores.min(), "max: %.1f" % test_scores.max())
+
+        if SAVE_MODEL:
+            saveScore(test_scores.mean())
+            saver.save(SESSION, model_savefile)
+            print("Saving the network weigths to:", model_savefile)
+            if test_scores.mean() > max_avgR:
+                max_avgR = test_scores.mean()
+                saver.save(SESSION, max_model_savefile)
+
+        print("Total ellapsed time: %.2f minutes" % ((time() - time_start) / 60.0))
+'''
+print("TIME TO WATCH!!")
+# Reinitialize the game with window visible
+game.close()
+game.set_window_visible(True)
+game.set_mode(Mode.ASYNC_PLAYER)
+game.init()
+score = []
+for _ in trange(EPISODE_TO_WATCH, leave=False):
+    game.new_episode()
+    agent.reset_cell_state()
+    while not game.is_episode_finished():
+        state = preprocess(game.get_state().screen_buffer)
+        action = agent.act(state, train=False)
+        game.set_action(actions[action])
+        for i in range(FRAME_REPEAT):
+            game.advance_action()
+            done = game.is_episode_finished()
+            if done:
+                break
+
+    # Sleep between episodes
+    sleep(1.0)
+    score.append(game.get_total_reward())
+score = np.array(score)
+game.close()
+print("Results: mean: %.1f±%.1f," % (score.mean(), score.std()),
+          "min: %.1f" % score.min(), "max: %.1f" % score.max())
+'''
+#just test
---
+++ mit-GameSimulator.py
@@ -0,0 +1,142 @@
+import skimage.color
+import skimage.transform
+from random import choices
+from random import randint
+import numpy as np
+
+class mit:
+    def __init__(self, filename):
+        self.file = filename
+        self.discount = 0.990000
+        self.states = 204
+        self.actions = 4
+        self.observations = 28
+        self.cur_state = 0
+        self.cur_observation = 0
+        self.state_list = [i for i in range(self.states)]
+        self.obs_list = [i for i in range(self.observations)]
+        self.T = np.zeros([self.actions, self.states, self.states], dtype=np.float32)
+        self.O = np.zeros([self.actions, self.states, self.observations], dtype=np.float32)
+        self.reward = np.zeros([self.actions, self.states], dtype=np.float32)
+        self.p_state = [0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000]
+        self.total_steps = 0
+        self.is_terminated = False
+        self.total_reward = 0
+        
+    def init_game(self):
+        file_reader = open(self.file)
+        lines = file_reader.readlines()
+        # 读取数据
+        print('Reading file....')
+        for line in lines:
+            line = line.strip()
+            # Transition Probabilities
+            if line[0]=='T':
+                items = line.split(':')
+                action = int(items[1].strip())
+                state = int(items[2].strip())
+                items = items[3].strip().split(' ')
+                n_state = int(items[0].strip())
+                p_state = float(items[1].strip())
+                self.T[action][state][n_state] = p_state
+            # Observation Probabilities
+            if line[0]=='O':
+                items = line.split(':')
+                action = int(items[1].strip())
+                state = int(items[2].strip())
+                items = items[3].strip().split(' ')
+                obs = int(items[0].strip())
+                p_state = float(items[1].strip())
+                self.O[action][state][obs] = p_state
+            # Rewards
+            if line[0]=='R':
+                items = line.split(':')
+                action = int(items[1].strip())
+                state = int(items[2].strip())
+                items = items[4].strip().split(' ')
+                r_state = float(items[1].strip())
+                self.reward[action][state] = r_state
+
+        print('Reading file done!')
+        self.reset()
+    
+    def reset(self):
+        print('Initailize start state...')
+        self.cur_state = choices(self.state_list, self.p_state, k=1)[0]
+        p_obs = self.O[randint(0,self.actions-1)][self.cur_state].flatten()
+        self.cur_observation = choices(self.obs_list, p_obs, k=1)[0]
+        self.total_reward = 0
+        self.total_steps = 0
+        self.is_terminated = False
+        print('Initailize done! Init_state:', self.cur_state, ' Init_obs:', self.cur_observation)
+                
+    def make_action(self, action):
+        self.total_steps += 1
+        if self.total_steps >= 250:
+            self.is_terminated = True
+            
+        if action<0 or action>=self.actions:
+            print('There is no this action:', action)
+                
+        p_trans = self.T[action][self.cur_state].flatten()
+        self.cur_state = choices(self.state_list, p_trans, k=1)[0]
+        p_obs = self.O[action][self.cur_state].flatten()
+        self.cur_observation = choices(self.obs_list, p_obs, k=1)[0]
+        cur_reward = self.reward[action][self.cur_state]
+        self.total_reward += cur_reward
+        # 如果到达目标地点，奖励值为1，判为终止状态
+        if cur_reward>0:
+            self.is_terminated = True
+            
+        return self.cur_observation, cur_reward, self.is_terminated
+    
+    def get_total_reward(self):
+        return self.total_reward
+
+class GameSimulator:
+    def __init__(self, frame_repeat=4):
+        self.game = None
+        self.frame_repeat = frame_repeat
+        
+    def initialize(self, filename="mit.POMDP"):
+        # 初始化游戏，返回游戏的动作数目
+        print("Initializing game...")
+        self.game = mit(filename)
+        self.game.init_game()
+        print("Game initialized.")
+        return self.game.actions
+
+    def get_state(self):
+        # 获取当前游戏的画面，游戏结束则获得空
+        obs_list = np.zeros([self.game.observations], dtype=np.int32).tolist()
+        obs_list[self.game.cur_observation] = 1
+        return obs_list
+    
+    def get_action_size(self):
+        # 获取动作数目
+        return self.game.actions
+    
+    def __get_button_size(self):
+        return self.game.actions
+    
+    def make_action(self, action):
+        # 执行动作
+        new_state, reward, done = self.game.make_action(action)
+        obs_list = np.zeros([self.game.observations], dtype=np.int32).tolist()
+        obs_list[new_state] = 1
+        return obs_list, reward, done
+    
+    def is_terminared(self):
+        # 判断游戏是否终止
+        return self.game.is_terminated
+    
+    def reset(self):
+        # 重新开始游戏
+        self.game.reset()
+    
+    def close(self):
+        # 关闭游戏模拟器
+        print('No this function.')
+        
+    def get_total_reward(self):
+        return self.game.get_total_reward()
