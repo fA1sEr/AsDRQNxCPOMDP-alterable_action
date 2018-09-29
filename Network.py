292e5d3c23bfdf75b3931a8fa962295f8082b9a9
zhangli <945560537@qq.com>
2018-09-29 19:30:39 +0800

    add debug message

--- Network.py
+++ Network.py
@@ -14,11 +14,11 @@ class Network:
         self.train_length = tf.placeholder(dtype=tf.int32)
         self.batch_size = tf.placeholder(dtype=tf.int32, shape=[])
 
-        flat = tf.contrib.layers.legacy_fully_connected(x=self.state, num_output_units=hidden_size)
+        slef.flat = tf.contrib.layers.legacy_fully_connected(x=self.state, num_output_units=hidden_size)
 
         self.cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_size, state_is_tuple=True)
 
-        self.fc_reshape = tf.reshape(flat, [self.batch_size, self.train_length, hidden_size])
+        self.fc_reshape = tf.reshape(self.flat, [self.batch_size, self.train_length, hidden_size])
         self.state_in = self.cell.zero_state(self.batch_size, tf.float32)
         self.rnn, self.rnn_state = tf.nn.dynamic_rnn(inputs=self.fc_reshape, cell=self.cell, dtype=tf.float32,
                                                      initial_state=self.state_in, scope=scope+'_rnn')
@@ -50,8 +50,14 @@ class Network:
                                                    self.batch_size: self.train_batch_size, self.state_in: state_in})
 
     def get_1q(self, state, state_in):
-        return self.session.run(self.q, feed_dict={self.state: state, self.train_length: 1,
+        res = self.session.run(self.q, feed_dict={self.state: state, self.train_length: 1,
                                                    self.batch_size: 1, self.state_in: state_in})
+        self.session.run(tf.Print(input_=self.state, data=[self.state], message='state:', summarize=10000))
+        self.session.run(tf.Print(input_=self.flat, data=[self.flat], message='flat:', summarize=10000))
+        self.session.run(tf.Print(input_=self.fc_reshape, data=[self.fc_reshape], message='fc_reshape:', summarize=10000))
+        self.session.run(tf.Print(input_=self.rnn, data=[self.rnn], message='rnn:', summarize=10000))
+        self.session.run(tf.Print(input_=self.q, data=[self.q], message='q:', summarize=10000))
+        return res
 
     def get_best_action(self, state, state_in):
         return self.session.run([self.best_a, self.rnn_state], feed_dict={self.state: [state], self.train_length: 1,
